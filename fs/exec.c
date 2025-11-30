98505cd194797414c0a3435f318467433444c2a3
Echo-Whale <echo_52hz@icloud.com>
2025-11-30 22:49:43 +0800

    fix: remove manual KSU hooks, use kprobes method instead
    The new KernelSU uses kprobes to automatically hook kernel functions,
    so manual hooks (ksu_handle_vfs_read, ksu_handle_execveat, etc.) are
    no longer needed and cause 'undefined symbol' errors during build.
    
    Removed manual hooks from:
    - fs/read_write.c (ksu_handle_vfs_read)
    - fs/exec.c (ksu_handle_execveat)
    - fs/open.c (ksu_handle_faccessat)
    - fs/stat.c (ksu_handle_stat)

--- fs/exec.c
+++ fs/exec.c
@@ -1891,8 +1891,6 @@ out_unmark:
 	return retval;
 }
 
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
 
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
@@ -1902,7 +1900,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct linux_binprm *bprm;
 	int retval;
 
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
